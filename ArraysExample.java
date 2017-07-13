aed8507c1a27999946dcdc6509dbc48c2c0cbd6b
marvin <marvin@debian.vinprojects>
2017-07-13 01:44:49 -0700

    Added more problems

---
+++ ArraysExample.java
@@ -0,0 +1,18 @@
+public class ArraysExample {
+
+  public static void printArray(int arr[][]) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[i].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+    }
+  }
+
+  public static void main(String args[]) {
+    int matrix[][] = {
+      {1, 2, 3, 4, 5},
+      {6, 7, 8, 9, 10}
+    };
+    printArray(matrix);
+  }
+}
